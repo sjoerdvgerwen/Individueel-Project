475de66f4841c88b512299cd57130022f14a0aee
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2020-12-13 21:19:51 +0100

    Minimal code cleanup and bootstrap added

---
+++ Practice/Models/NewProductViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Warehouse.Webapp.Models
+{
+    public class NewProductViewModel
+    {
+        public Guid ProductID { get; set; }
+        
+        public string ProductName { get; set; }
+        
+        public string ProductDescription { get; set; }
+        
+        public int ProductQuantity { get; set; }
+    }
+}
